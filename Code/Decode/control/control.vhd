446603432d42e9479c588319f29c01625e98102d
ranamoataz3 <ranamoataz02@gmail.com>
2023-04-20 00:01:30 +0200

    control unit to be tested pt 2

--- Code/Decode/control/control.vhd
+++ Code/Decode/control/control.vhd
@@ -30,20 +30,22 @@ architecture Behavioral of Control_Unit is
 
 begin
     alu_enable <= '0' when opcode = "111001" else not opcode(5);
-    branch_enable <= not (opcode(5) or opcode(4) or opcode(3));
+    branch_enable <= '0' when opcode = "111001" else not (opcode(5) or opcode(4) or opcode(3));
     part_selector <= opcode(4 downto 3);
     op_selector <= opcode(2 downto 0);
-    call_operation <= '1' when opcode = "111111" else '0';
-    read_addr_selector <= (opcode(5) and opcode(4));
-    write_addr_selector <= (opcode(5) and opcode(4));
-    data_written_selector <= opcode(0);
-    mem_op <= (opcode(5) and (opcode(4) or  opcode(3)));
-    mem_read <=((opcode(5) xnor opcode(4) xnor opcode(3) xnor opcode(2) xnor opcode(1) xnor opcode(0)) and (opcode(5) and (opcode(4) or  opcode(3))));
-    mem_write<=((opcode(5) xor opcode(4) xor opcode(3) xor opcode(2) xor opcode(1) xor opcode(0)) and (opcode(5) and (opcode(4) or  opcode(3))));
-    data_bus_selector <= opcode(5) and opcode(4) and opcode(3) and opcode(2) and opcode(1) and opcode(0);
-    wb_data_selector <= opcode(5);
-    wb_flag_selector <= opcode(5);
-    in_port_data_selector<= '1' when (opcode="100001");
+    call_operation <= '0' when opcode = "111001" else '1' when opcode = "111111" else '0';
+    read_addr_selector <= '0' when opcode = "111001" else (opcode(5) and opcode(4));
+    write_addr_selector <= '0' when opcode = "111001" else (opcode(5) and opcode(4));
+    data_written_selector <= '0' when opcode = "111001" else opcode(0);
+    mem_op <= '0' when opcode = "111001" else (opcode(5) and (opcode(4) or  opcode(3)));
+    mem_read <= '0' when opcode = "111001" else ((opcode(5) xnor opcode(4) xnor opcode(3) xnor opcode(2) xnor opcode(1) xnor opcode(0)) and (opcode(5) and (opcode(4) or  opcode(3))));
+    mem_write<= '0' when opcode = "111001" else ((opcode(5) xor opcode(4) xor opcode(3) xor opcode(2) xor opcode(1) xor opcode(0)) and (opcode(5) and (opcode(4) or  opcode(3))));
+    data_bus_selector <= '0' when opcode = "111001" else opcode(5) and opcode(4) and opcode(3) and opcode(2) and opcode(1) and opcode(0);
+    wb_data_selector <= '0' when opcode = "111001" else opcode(5);
+    wb_flag_selector <= '0' when opcode = "111001" else opcode(5);
+    in_port_data_selector<='1' when (opcode="100001");
     reg_file_enable <= '1' when (opcode(5 downto 3) = "010" or opcode(5 downto 3) = "011" or opcode = "100001");
     flag_enable <= '1' when (opcode = "000000" or opcode = "000001" or opcode(5 downto 3) = "011");
+    propagated_ret_rti <= '0' when opcode = "111001" else (opcode(5) and opcode(4) and  opcode(3) and opcode(2));
+    current_ret_rti <= '1' when opcode = "111001" else (opcode(5) and opcode(4) and  opcode(3) and opcode(2));
 end Behavioral;
