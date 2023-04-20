0bf2fc24d20f2530f978a44d55fecd343813519c
Ola abouelhadid <abouelhadid.ola@gmail.com>
2023-04-20 06:34:28 +0200

    reg file

--- Code/Decode/control/regFile.vhd
+++ Code/Decode/control/regFile.vhd
@@ -22,42 +22,23 @@ architecture behavioral of regFile is
     type mem_array_t is array (0 to 7) of std_logic_vector(15 downto 0);
     signal memory_array: mem_array_t;
 
-    signal write_address_internal: unsigned(2 downto 0);
-    signal read_addressDes_internal: unsigned(2 downto 0);
-    signal read_address1_internal: unsigned(2 downto 0);
-    signal read_address2_internal: unsigned(2 downto 0);
-    
-    signal data_read1_internal: std_logic_vector(15 downto 0);
-    signal data_read2_internal: std_logic_vector(15 downto 0);
-    signal data_readDes_internal: std_logic_vector(15 downto 0);
     begin
 
     process (clk,reset)
     begin
         if reset = '1' then
-            memory_array <= (others => (others => '0'));
-            rdData <= (others => '0');
-            rs1Data <= (others => '0');
-            rs2Data <= (others => '0');
-        elsif rising_edge(clk) then
-            if regFileEnable = '1' then
-                write_address_internal <= (unsigned(WBAddress));
-                memory_array(to_integer(write_address_internal)) <= WBData;
-            end if;
-
-            read_address1_internal <= (unsigned(rs1Address));
-            read_address2_internal <= (unsigned(rs2Address));
-            read_addressDes_internal <= (unsigned(rdAddress));
-
-            data_read1_internal <= memory_array(to_integer(read_address1_internal));
-            data_read2_internal <= memory_array(to_integer(read_address2_internal));
-            data_readDes_internal <= memory_array(to_integer(read_addressDes_internal));
-
+            for i in 0 to 7 loop
+                memory_array(i) <= (others => '0');
+            end loop;  
+        elsif falling_edge(clk) then
+                if regFileEnable = '1' then
+                    memory_array(to_integer(unsigned(WBAddress))) <= WBData;
+                end if;
         end if;
     end process;
-        rdData <= data_readDes_internal;
-        rs1Data <= data_read1_internal;
-        rs2Data <= data_read2_internal;
+        rdData <= memory_array(to_integer(unsigned(rdAddress)));
+        rs1Data <= memory_array(to_integer(unsigned(rs1Address)));
+        rs2Data <= memory_array(to_integer(unsigned(rs2Address)));
 end behavioral;
 
 
